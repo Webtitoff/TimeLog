2b0a592fe0d458e5bfd5d783b376bbc16abfa288
V-twin <v-twin1100@yandex.ru>
2017-10-01 17:00:49 +0300

    The first commit with chanks

---
+++ core/cache/mgr/smarty/default/f009a60e5ae83bd0ed2f18087da6ebd0afea40c1_0.file.update.tpl.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version 3.1.27, created on 2017-10-01 15:15:49
+         compiled from "E:\OSPanel\domains\timelog\manager\templates\default\element\chunk\update.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:2479759d0dc756120c4_02315504%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'f009a60e5ae83bd0ed2f18087da6ebd0afea40c1' => 
+    array (
+      0 => 'E:\\OSPanel\\domains\\timelog\\manager\\templates\\default\\element\\chunk\\update.tpl',
+      1 => 1505291563,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2479759d0dc756120c4_02315504',
+  'variables' => 
+  array (
+    'onChunkFormPrerender' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_59d0dc75613be2_33402117',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_59d0dc75613be2_33402117')) {
+function content_59d0dc75613be2_33402117 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '2479759d0dc756120c4_02315504';
+?>
+<div id="modx-panel-chunk-div"></div>
+<?php echo $_smarty_tpl->tpl_vars['onChunkFormPrerender']->value;
+
+}
+}
+?>
